be983b38a83ecd02bb0c0485dcc3d9cb620d8182
Yuangang Sheng <yuangang.sheng@intive.com>
2023-11-03 11:27:49 +0100

    Create project

---
+++ TCASample/TCASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASampleApp.swift
+//  TCASample
+//
+//  Created by Yuangang Sheng on 03.11.23.
+//
+
+import SwiftUI
+
+@main
+struct TCASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
